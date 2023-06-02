547a14709c38b673a4e96f5e2dfc605ce14f5929
Tomas Campusano <tomascampusano.10@gmail.com>
2023-06-02 01:58:14 -0300

    Finished: presentation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgIconsModule } from '@ng-icons/core';
+import { matLocationOn } from '@ng-icons/material-icons/baseline'
+import { bootstrapLinkedin, bootstrapGithub } from '@ng-icons/bootstrap-icons'
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PresentationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgIconsModule.withIcons({ matLocationOn, bootstrapLinkedin, bootstrapGithub })
   ],
   providers: [],
   bootstrap: [AppComponent]
